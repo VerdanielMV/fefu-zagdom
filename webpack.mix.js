f56f91c17ff74bd1573596ab276c8a88df82572d
Verdaniel <denmor2002@gmail.com>
2022-04-02 12:05:07 +1000

    Третье задание

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,11 +1,16 @@
 const mix = require('laravel-mix');
+const {resolve} = require('path');
 
 mix
     .setPublicPath('public')
     .js('resources/scripts/main.js', 'build/bundle.js')
-    .sass('resources/styles/main.scss', 'build/bundle.css')
+    .sass('resources/styles/main.scss', 'build/bundle.css',  {}, [
+        require('postcss-axis')
+    ])
     .browserSync({
         watch: true,
         proxy: 'localhost',
         files: ['resources/**/*']
     });
+
+    mix.alias({'uikit-util': resolve(__dirname, 'node_modules/uiit/src/js/util')});
